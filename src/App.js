6c607f3c1408d1f056aeb27f5e6c931ef1714406
Piash <nazem.piash10@gmail.com>
2022-02-03 02:27:55 +0600

    pusher implement

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, {useEffect} from "react";
+import Pusher from "pusher-js";
+
+import axios from "./config/axios.js";
 import './App.css';
 import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
 import Chat from "./components/Chat/Chat";
 
 function App() {
+    useEffect(() => {
+        // no dependency, run once
+        const pusher = new Pusher('42c0d3dbf431edb54c57', {
+            cluster: 'mt1'
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', function(data) {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
   return (
     <div className="App">
       <div className="main__body"> 
